1d2df36f7de05a6cf3f74fdfc64bc963a5292c17
diopmorfall <morfall.papydiop99@gmail.com>
2024-02-06 15:18:48 +0000

    Handling of the API response

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,17 +9,32 @@ export default function HomePage() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     const [recipes, setRecipes] = useState([]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const API_KEY = import.meta.env.VITE_API_KEY;
 
     const navigate = useNavigate()
     const meals = ["Breakfast", "Main course", "Appetizer", "Side dish", "Dessert", "Drink"];
 
+    useEffect(() => {
+        setWindowWidth(window.innerWidth);
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    function setRecipeArrayLength(){
+        return windowWidth < 768
+            ? 4
+            : windowWidth < 1024
+                ? 6
+                : 8
+    }
+
     async function getRandomRecipes() {
         setIsLoading(true);
         const res = await axios.get(
-            `https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&diet=vegetarian|vegan`
+            `https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&diet=vegetarian|vegan&number=${setRecipeArrayLength()}`
         ).catch(error => console.log(error))
-        console.log(res.data);
         setRecipes(res.data.results);
         setIsLoading(false);
     }
@@ -39,10 +54,10 @@ export default function HomePage() {
             <p className='text-lg font-semibold md:text-lg md:my-4'>Look for vegetarian and vegan recipes...</p>
             <SearchBar />
             <p className='text-lg font-semibold md:text-lg md:my-4'>...or search them by course...</p>
-            <div className='card-courses grid grid-cols-2 gap-6 w-4/5 p-2 my-6 md:w-3/5 lg:grid-cols-3 lg:w-3/4'>
+            <div className='card-courses grid grid-cols-2 gap-6 w-4/5 p-2 my-6 md:grid-cols-3 md:w-4/5 lg:grid-cols-3 lg:w-3/4 lg:gap-x-16 lg:gap-y-10'>
                 {meals.map(meal => 
                     <div key={meal} className='w-full h-[200px] flex flex-col justify-between items-center pb-4 rounded-xl bg-blue-400
-                        md:h-[300px]
+                        md:h-[250px]
                     '>
                         <img src="" alt="Meal image" />
                         <p className='md:text-xl lg:text-2xl'>{meal}</p>
@@ -54,16 +69,18 @@ export default function HomePage() {
             {!isLoading && error ? <p className='text-red-400'>Error: {error}</p> : ''}
             {!isLoading && recipes.length > 0 ?
                 (
-                    recipes.map(recipe => 
-                        <RecipeCard
-                            key={recipe.id}
-                            id={recipe.id}
-                            title={recipe.title}
-                            servings={recipe.servings}
-                            readyInMinutes={recipe.readyInMinutes}
-                            image={recipe.image}
-                        />             
-                    )
+                    <div className='w-full flex flex-col items-center md:w-[90%] md:grid md:grid-cols-3 md:gap-x-8 lg:w-[85%] lg:grid lg:grid-cols-4 lg:gap-x-10 lg:py-6'>
+                        {recipes.map(recipe => 
+                            <RecipeCard
+                                key={recipe.id}
+                                id={recipe.id}
+                                title={recipe.title}
+                                servings={recipe.servings}
+                                readyInMinutes={recipe.readyInMinutes}
+                                image={recipe.image}
+                            />
+                        )}
+                    </div>
                 ) : <p className='text-center px-8 font-semibold text-red-400 md:px-16 md:text-lg'>
                         We're sorry we couldn't give you recipes suggestions. Please search for the recipes you're interest to
                     </p>
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -16,14 +16,14 @@ export default function RecipeCard({
     }
 
     return (
-        <div className="w-2/3 h-[350px] flex flex-col items-center justify-around text-center p-4 rounded-xl border-2 md:w-1/2 md:text-lg lg:w-1/4">
-            <img className='w-full' src={`${baseUri}${image}`} alt={title} />
-            <h2 className='font-bold'>{title}</h2>
-            <p>Servings: {servings}</p>
-            <h4>Ready in {readyInMinutes} minutes</h4>
-            <p className="" onClick={getRecipeData}>
-                See how to cook it
-            </p>
+        <div onClick={getRecipeData}
+            className="w-2/3 h-[350px] flex flex-col items-center justify-between text-center pb-4 my-6 rounded-xl border-2
+            md:w-full md:text-lg lg:w-full"
+        >
+            <img className='w-full h-1/2 rounded-t-xl' src={`${baseUri}${image}`} alt={title} />
+            <h2 className='font-bold px-4'>{title}</h2>
+            <p>{servings} servings</p>
+            <h4>{readyInMinutes} minutes</h4>
         </div>
     );
 }
